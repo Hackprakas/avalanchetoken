4aa21bdaf5b94e2c674fa47ea2b6a892f8a63e0a
PrakashDev <prakash.t.r.2021.cse@ritchennai.edu.in>
2023-09-24 04:54:20 +0000

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,48 @@
+
+require("@nomicfoundation/hardhat-toolbox");
+
+const FORK_FUJI = false;
+const FORK_MAINNET = false;
+let forkingData = undefined;
+
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [
+       'c4acd5fa1ad13df8ee866c290c81733c4521ec0206b4b5de1ec828b3f801c605'
+      ]
+    },
+    mainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [
+        'c4acd5fa1ad13df8ee866c290c81733c4521ec0206b4b5de1ec828b3f801c605'
+      ]
+    }
+  },
+  etherscan:{
+    apikey:"IWNIDIIGXK74NTT6VJ98925CDRVGDJNQH1"
+  },
+}
---
+++ test/Lock.js
@@ -0,0 +1,56 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+
+
+describe("DegenToken", function () {
+  let owner;
+  let user1;
+  let degenToken;
+
+  
+
+  beforeEach(async () => {
+    [owner, user1] = await ethers.getSigners();
+
+    const DegenToken = await ethers.getContractFactory("DegenToken");
+    degenToken = await DegenToken.deploy();
+    // await degenToken.deployed();
+  });
+
+  it("Should deploy the contract with the correct name and symbol", async function () {
+    expect(await degenToken.name()).to.equal("Degen");
+    expect(await degenToken.symbol()).to.equal("DGN");
+  });
+
+  it("Should mint tokens to the owner", async function () {
+    const initialBalance = await degenToken.balanceOf(owner.address);
+    const amountToMint = ethers.parseEther("2"); // Mint 1000 DGN (adjust as needed)
+
+    await degenToken.connect(owner).mint(owner.address, amountToMint);
+
+    const finalBalance = await degenToken.balanceOf(owner.address);
+
+    const balanceChange = finalBalance-(initialBalance);
+
+    // Convert amountToMint to BigNumber for comparison
+    const expectedChange = ethers.parseEther("2");
+
+    // Compare the balance change with the expected change
+    expect(balanceChange).to.equal(expectedChange);
+  });
+
+  it("Should not allow minting by non-owner", async function () {
+    const nonOwner = user1;
+    const amountToMint = ethers.parseEther("2"); // Mint 1000 DGN (adjust as needed)
+
+    await expect(degenToken.connect(nonOwner).mint(owner.address, amountToMint)).to.be.revertedWith(
+      "Ownable: caller is not the owner"
+    );
+  });
+});
